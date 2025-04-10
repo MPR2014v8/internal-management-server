f84f0b0579e32b54978c64ac198675512b849600
blacksnow <domezaza48@gmail.com>
2025-04-10 17:48:18 +0700

    Comment out unused TimeStamp type definition in time-stamp.schema.ts

--- src/time-stamp/schema/time-stamp.schema.ts
+++ src/time-stamp/schema/time-stamp.schema.ts
@@ -2,4 +2,4 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { Document } from 'mongoose';
 import * as bcrypt from 'bcrypt';
 
-export type TimeStamp = User & Document;
+// export type TimeStamp = User & Document;
